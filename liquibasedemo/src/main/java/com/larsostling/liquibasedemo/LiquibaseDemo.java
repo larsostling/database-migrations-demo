b77de3c54a0f99098f90378893ee2e19b09eb7c1
Lars Östling <lars.ostling@jayway.com>
2015-05-10 14:53:51 +0200

    Add simple project and examples for liquibase

---
+++ liquibasedemo/src/main/java/com/larsostling/liquibasedemo/LiquibaseDemo.java
@@ -0,0 +1,12 @@
+package com.larsostling.liquibasedemo;
+
+/**
+ * Simple demo for running liquibase from java code.
+ */
+public class LiquibaseDemo {
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+
+}
