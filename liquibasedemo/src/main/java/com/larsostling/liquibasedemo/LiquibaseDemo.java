347c67f3f206452b31f0d62d397d022900abab4d
Lars Östling <lars.ostling@jayway.com>
2015-05-10 20:36:28 +0200

    Add custom java migration for liquibase

--- liquibasedemo/src/main/java/com/larsostling/liquibasedemo/LiquibaseDemo.java
+++ liquibasedemo/src/main/java/com/larsostling/liquibasedemo/LiquibaseDemo.java
@@ -5,7 +5,7 @@ package com.larsostling.liquibasedemo;
  */
 public class LiquibaseDemo {
 
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         System.out.println( "Hello World!" );
     }
 
---
+++ liquibasedemo/src/main/java/com/larsostling/liquibasedemo/liquibase/AddUniqueIdToCustomer.java
@@ -0,0 +1,63 @@
+package com.larsostling.liquibasedemo.liquibase;
+
+import liquibase.change.custom.CustomTaskChange;
+import liquibase.database.Database;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.CustomChangeException;
+import liquibase.exception.SetupException;
+import liquibase.exception.ValidationErrors;
+import liquibase.resource.ResourceAccessor;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.UUID;
+
+public class AddUniqueIdToCustomer implements CustomTaskChange {
+
+    private static String ADD_COLUMN_UNIQUE_ID = "alter table customer add uniqueid varchar(36)";
+    private static String SELECT_ID_FROM_CUSTOMER = "select id from customer";
+    private static String UPDATE_TABLE_STATEMENT = "update customer set uniqueid = ? where id = ?";
+
+    @Override
+    public void execute(Database database) throws CustomChangeException {
+        Connection connection = ((JdbcConnection) database.getConnection()).getUnderlyingConnection();
+        try {
+            try (Statement statement = connection.createStatement()) {
+                statement.execute(ADD_COLUMN_UNIQUE_ID);
+            }
+            try (Statement statement = connection.createStatement();
+                 ResultSet rs = statement.executeQuery(SELECT_ID_FROM_CUSTOMER)) {
+                while (rs.next()) {
+                    try (PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_TABLE_STATEMENT)) {
+                        preparedStatement.setString(1, UUID.randomUUID().toString());
+                        preparedStatement.setInt(2, rs.getInt("id"));
+                        preparedStatement.execute();
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new CustomChangeException("SQL error adding unique id to customer", e);
+        }
+    }
+
+    @Override
+    public String getConfirmationMessage() {
+        return null;
+    }
+
+    @Override
+    public void setUp() throws SetupException {
+    }
+
+    @Override
+    public void setFileOpener(ResourceAccessor resourceAccessor) {
+    }
+
+    @Override
+    public ValidationErrors validate(Database database) {
+        return null;
+    }
+}
