452077bb90dd444e45644ce853a0933acdfa1dbd
Lars Östling <lars.ostling@jayway.com>
2015-05-10 21:42:54 +0200

    Add example for running liquibase from code

--- liquibasedemo/src/main/java/com/larsostling/liquibasedemo/LiquibaseDemo.java
+++ liquibasedemo/src/main/java/com/larsostling/liquibasedemo/LiquibaseDemo.java
@@ -1,12 +1,55 @@
 package com.larsostling.liquibasedemo;
 
+import liquibase.Contexts;
+import liquibase.database.Database;
+import liquibase.database.DatabaseConnection;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.DatabaseException;
+import liquibase.Liquibase;
+import liquibase.resource.ClassLoaderResourceAccessor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Simple demo for running liquibase from java code.
  */
 public class LiquibaseDemo {
 
-    public static void main(String[] args) {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) throws Exception {
+        Connection connection = null;
+        Liquibase liquibase = null;
+        try {
+            connection = createConnection();
+            liquibase = new Liquibase("liquibase/changelog.xml",
+                    new ClassLoaderResourceAccessor(),
+                    createDataBase(connection));
+            liquibase.update((Contexts) null);
+        } finally {
+            if (liquibase != null) {
+                liquibase.forceReleaseLocks();
+            }
+            if (connection != null) {
+                try {
+                    connection.rollback();
+                    connection.close();
+                } catch (SQLException e) {
+                    //nothing to do
+                }
+            }
+        }
+    }
+
+    private static Connection createConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        return DriverManager.getConnection("jdbc:h2:file:target/liquibasedemo", "sa", null);
+    }
+
+    private static Database createDataBase(Connection connection) throws DatabaseException {
+        DatabaseConnection liquibaseConnection = new JdbcConnection(connection);
+        return DatabaseFactory.getInstance().findCorrectDatabaseImplementation(liquibaseConnection);
     }
 
 }
